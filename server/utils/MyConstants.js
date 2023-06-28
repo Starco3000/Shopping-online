72a34825294170c5da0ba912361eb83c49ca11ef
Nguyen Duc Minh <minhnguyen9686q6@gmail.com>
2023-06-28 12:29:59 +0700

    initial-update

---
+++ server/utils/MyConstants.js
@@ -0,0 +1,11 @@
+const MyConstants = {
+    DB_SERVER: 'clustersgp.czcorfr.mongodb.net', /*mongodb+srv://minhnguyen:web0201@clustersgp.czcorfr.mongodb.net/?retryWrites=true&w=majority*/
+    DB_USER: 'minhnguyen',
+    DB_PASS: 'web0201',
+    DB_DATABASE: 'shoppingonline',
+    EMAIL_USER: 'marcus.nguyenq6@gmail.com', // Google mail
+    EMAIL_PASS: 'zixbaulvfowhcacv', //app-pass
+    JWT_SECRET: 'myfavoritecolorisred', //<jwt_secret>
+    JWT_EXPIRES: '86400000',//'<jwt_expires>', // in milliseconds // 86400000 equal 24hrs
+  };
+  module.exports = MyConstants;
